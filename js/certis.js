fb5f2e0299cf2822e044255bbe72794a90e00dcf
Sebastián Arias <sebastian.arias0416@gmail.com>
2026-07-27 16:32:10 -0600

    Creacion de Certificaciones

---
+++ js/certis.js
@@ -0,0 +1,8 @@
+function openModal(imgSrc) {
+  document.getElementById("modal").style.display = "block";
+  document.getElementById("modal-img").src = imgSrc;
+}
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
